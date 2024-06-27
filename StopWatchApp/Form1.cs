0536342b6dfb9014c16b0820f21b1b9fc08c0cc0
GasanaJr <gasanadj2003@gmail.com>
2024-06-27 17:44:38 +0200

    feat(stopwatch): initialize the constructor and add the start and tick function

---
+++ StopWatchApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace StopWatchApp
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch;
+        private System.Windows.Forms.Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += timertick;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            timer.Start();
+        }
+
+
+        private void timertick(object sender, EventArgs e)
+        {
+            TimeSpan Span = stopwatch.Elapsed;
+            label2.Text = String.Format("{0:00}:{1:00}:{2:00}", Span.Hours, Span.Minutes, Span.Seconds);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
